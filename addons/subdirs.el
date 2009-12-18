38f28e2b7550a8f7b8c158462c6735b39c7d1712
Sean Xue <lianggui.xue@gmail.com>
2009-12-18 12:48:30 +0800

    add addons to work dir, some ignored according .gitingnore

---
+++ addons/subdirs.el
@@ -0,0 +1,6 @@
+(defun my-add-subdirs-to-load-path (dir)
+(let ((default-directory (concat dir "/")))
+(setq load-path (cons dir load-path))
+(normal-top-level-add-subdirs-to-load-path)))
+
+(my-add-subdirs-to-load-path "~/pro/emacs/addons")

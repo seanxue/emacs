262745a67ffca02a9b6b6ac0dd375d20869cd9a5
Sean Xue <lianggui.xue@gmail.com>
2009-12-23 23:38:06 +0800

    update config of ccmode and semantic

--- config/rc-ccmode.el
+++ config/rc-ccmode.el
@@ -2,4 +2,12 @@
 
 (require 'cc-mode)
 
+(defun sean/c-mode-common-hook()
+  (setq tab-width 4 indent-tabs-mode nil)
+  (c-toggle-auto-hungry-state 1) ; hungry-delete and auto-newline
+  (define-key c-mode-base-map [(tab)] 'sean/indent-or-complete)
+  (define-key c-mode-base-map [(meta ?/)] 'semantic-ia-complete-symbol-menu)
+)
+
+(add-hook 'c-mode-common-hook 'sean/c-mode-common-hook)
 ;;; rc-ccmode.el ends here

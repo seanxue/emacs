9f05ffe4885edf331922a9f8025b0fa5ff70bb90
Sean Xue <lianggui.xue@gmail.com>
2010-03-21 11:08:57 +0800

    add google-c-style support

--- config/rc-ccmode.el
+++ config/rc-ccmode.el
@@ -1,13 +1,15 @@
 ;;; rc-ccmode.el 
 
 (require 'cc-mode)
+(require 'google-c-style)
 
 (defun sean/c-mode-common-hook()
-  (setq tab-width 4 indent-tabs-mode nil)
   (c-toggle-auto-hungry-state 1) ; hungry-delete and auto-newline
   (define-key c-mode-base-map [(tab)] 'sean/indent-or-complete)
   (define-key c-mode-base-map [(meta ?/)] 'semantic-ia-complete-symbol-menu)
 )
-
 (add-hook 'c-mode-common-hook 'sean/c-mode-common-hook)
+
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
 ;;; rc-ccmode.el ends here

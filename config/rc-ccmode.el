a487345064d71f8eada7c6d698b3682a85f1d3b4
Sean Xue <lianggui.xue@gmail.com>
2010-03-24 20:54:37 +0800

    modify c-common-hool to support google-c-style

--- config/rc-ccmode.el
+++ config/rc-ccmode.el
@@ -5,11 +5,10 @@
 
 (defun sean/c-mode-common-hook()
   (c-toggle-auto-hungry-state 1) ; hungry-delete and auto-newline
-  (define-key c-mode-base-map [(tab)] 'sean/indent-or-complete)
-  (define-key c-mode-base-map [(meta ?/)] 'semantic-ia-complete-symbol-menu)
-)
-(add-hook 'c-mode-common-hook 'sean/c-mode-common-hook)
+  (add-hook 'c-mode-common-hook 'google-set-c-style)
+  (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+;  (define-key c-mode-base-map [(tab)] 'sean/indent-or-complete)
+  (define-key c-mode-base-map [(meta ?/)] 'semantic-ia-complete-symbol-menu))
 
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(add-hook 'c-mode-common-hook 'sean/c-mode-common-hook)
 ;;; rc-ccmode.el ends here
